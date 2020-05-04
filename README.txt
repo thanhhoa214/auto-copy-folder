69e8ed27358cb8b0f0916e2a3965397dd6fb4fc5
Nguyễn Hòa <thanhthanhhoa123@gmail.com>
2020-05-04 19:19:06 +0700

    First commit

---
+++ README.txt
@@ -0,0 +1,15 @@
+***********************************
+****WELCOME TO AUTO-COPY_FOLDER****
+***********************************
+****Crafted by Nguyễn Thanh Hòa****
+***********05/04/2020**************
+***********************************
+
+1. Chỉnh sửa cài đặt thông qua: 
+    Nhấp đúp [ConfigAutoCopyFolder.exe] (Mọi thiết lập sẽ tự động lưu).
+
+2. Khởi động chạy ngầm (chỉ chạy 1 lần duy nhất): 
+    Nhấp đúp [src] để mở thư mục src.
+    Nhấp đúp [_RUN-AUTO.bat] để khởi động.
+
+Nếu có lỗi phát sinh, liên hệ 0979210499.
---
+++ src/execute.bat
@@ -0,0 +1,9 @@
+xcopy "E:\fpt\Spring 2020\C#.NET\slides" "E:\tripleh402\test23\slides\" /i /e 
+xcopy "E:\fpt\Spring 2020\C#.NET\slides" "D:\test\slides\" /i /e 
+xcopy "E:\fpt\Spring 2020\C#.NET\slides" "E:\tripleh402\thunghiem\slides\" /i /e 
+xcopy "C:\Users\ASUS\Downloads\Pubs Database" "E:\tripleh402\test23\Pubs Database\" /i /e 
+xcopy "C:\Users\ASUS\Downloads\Pubs Database" "D:\test\Pubs Database\" /i /e 
+xcopy "C:\Users\ASUS\Downloads\Pubs Database" "E:\tripleh402\thunghiem\Pubs Database\" /i /e 
+xcopy "C:\Users\ASUS\Downloads\student-management-Chuong\student-management\db" "E:\tripleh402\test23\db\" /i /e 
+xcopy "C:\Users\ASUS\Downloads\student-management-Chuong\student-management\db" "D:\test\db\" /i /e 
+xcopy "C:\Users\ASUS\Downloads\student-management-Chuong\student-management\db" "E:\tripleh402\thunghiem\db\" /i /e 
